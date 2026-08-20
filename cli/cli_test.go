24805a3dd724f96c884086f54ae38a26f46ad5f0
Bernd Zeimetz <bernd@bzed.de>
2026-08-20 20:33:20 +0200

    docs: Add example systemd service and socket files

--- cli/cli_test.go
+++ cli/cli_test.go
@@ -1,42 +1,9 @@
 package cli
 
 import (
-	"flag"
-	"os"
 	"testing"
 )
 
-
 func TestInfo(t *testing.T) {
 	info("localhost:9090")
-}
-
-
-func TestMainHelp(t *testing.T) {
-	oldArgs := os.Args
-	defer func() { os.Args = oldArgs }()
-	os.Args = []string{"pgproxy", "help"}
-
-	// mock config file creation
-	configFile := "test_pgproxy.conf"
-	os.WriteFile(configFile, []byte(`
-[Server]
-ProxyAddr = "localhost:9090"
-
-[DB]
-[DB.master]
-Addr = "127.0.0.1:5432"
-User = "postgres"
-Password = "testpass"
-DBName = "testdb"
-`), 0644)
-	defer os.Remove(configFile)
-
-	// Test Main with help
-	flag.CommandLine = flag.NewFlagSet(os.Args[0], flag.ExitOnError)
-	Main(configFile, []string{"pgproxy", "help"})
-
-	// Test Main with insufficient args
-	flag.CommandLine = flag.NewFlagSet(os.Args[0], flag.ExitOnError)
-	Main(configFile, []string{"pgproxy"})
 }
